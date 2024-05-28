278dcdbbd01bf32cc9b4df4c6123c100ed27219f
Joss Rider <jossrider@outlook.com>
2024-05-27 21:48:35 -0400

    Agregamos componentes Html a la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import styles from "./App.module.css"
+import Form from "./components/Form/Form"
 function App() {
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
       <div className={styles.container}>
-        <p>1</p>
-        <p>2</p>
+        <Form />
       </div>
     </>
   )
