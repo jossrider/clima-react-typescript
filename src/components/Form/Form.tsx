ff2240b9ea64068735ab4609816d0cdb523286ab
Joss Rider <jossrider@outlook.com>
2024-05-28 22:07:01 -0400

    Agregamos mas elementos html y css

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,42 @@
+import { ChangeEvent, useState } from "react"
+import type { SearchType } from "../../types"
 import { countries } from "../../data/countries"
+import styles from "./Form.module.css"
 
 export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  })
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    })
+  }
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.field}>
         <label htmlFor="city">Ciudad: </label>
-        <input type="text" id="city" name="city" placeholder="Ciudad" />
+        <input
+          type="text"
+          id="city"
+          name="city"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
       </div>
 
-      <div>
-        <label htmlFor="city">Pais: </label>
-        <select name="" id="">
+      <div className={styles.field}>
+        <label htmlFor="country">Pais: </label>
+        <select
+          id="country"
+          value={search.country}
+          name="country"
+          onChange={handleChange}>
           <option value="">-- Seleccione un Pais --</option>
           {countries.map((country) => (
             <option key={country.code} value={country.code}>
@@ -19,7 +45,7 @@ export default function Form() {
           ))}
         </select>
       </div>
-      <input type="submit" value="Consultar Clima" />
+      <input type="submit" value="Consultar Clima" className={styles.submit} />
     </form>
   )
 }
