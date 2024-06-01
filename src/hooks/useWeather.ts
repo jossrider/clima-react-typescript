0bfa5017a2e7e72edc730b013954af24e41ae7a4
Joss Rider <jossrider@outlook.com>
2024-05-31 22:11:10 -0400

    Agregamos inferimos el type de la respuesta de la api

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,30 @@
 import axios from "axios"
+import { z } from "zod"
 import { SearchType } from "../types"
+
+// TYPE GUARD O ASSERTION
+// function isWWeatherResponse(weather: unknown): weather is Weather {
+//   return (
+//     Boolean(weather) &&
+//     typeof weather === "object" &&
+//     typeof (weather as Weather).name === "string" &&
+//     typeof (weather as Weather).main.temp === "number" &&
+//     typeof (weather as Weather).main.temp_max === "number" &&
+//     typeof (weather as Weather).main.temp_min === "number"
+//   )
+// }
+
+// Zod
+const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number(),
+  }),
+})
+
+type Weather = z.infer<typeof Weather>
 
 export default function useWeather() {
   const appId = import.meta.env.VITE_API_KEY
@@ -9,6 +34,25 @@ export default function useWeather() {
       const { data } = await axios(geoUrl)
       const lat = data[0].lat
       const lon = data[0].lon
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+      // Castear el type
+      // const { data: weatherResult } = await axios<Weather>(weatherUrl)
+
+      // Type Guards
+      // const { data: weatherResult } = await axios(weatherUrl)
+      // const result = isWWeatherResponse(weatherResult)
+      // if (result) {
+      //   console.log(weatherResult.name)
+      // }
+
+      // Zod
+      const { data: weatherResult } = await axios<Weather>(weatherUrl)
+      const result = Weather.safeParse(weatherResult)
+      if (result.success) {
+        console.log(result.data.name)
+        console.log(result.data.main.temp)
+      }
     } catch (error) {
       console.log(error)
     }
