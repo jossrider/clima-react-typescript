f6857f7145c2ad036816fe5b648c0898851063f2
Joss Rider <jossrider@outlook.com>
2024-06-01 23:18:28 -0400

    Finalizamos el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import styles from "./App.module.css"
+import Alert from "./components/Alert/Alert"
 import Form from "./components/Form/Form"
+import Spinner from "./components/Spinner/Spinner"
+import WeatherDetail from "./components/WeatherDetail/WeatherDetail"
 import useWeather from "./hooks/useWeather"
 
 function App() {
-  const { fetchWeather } = useWeather()
+  const { weather, loading, notfound, fetchWeather, hasWeatherData } =
+    useWeather()
 
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
       <div className={styles.container}>
         <Form fetchWeather={fetchWeather} />
+        {loading && <Spinner />}
+        {hasWeatherData && <WeatherDetail weather={weather} />}
+        {notfound&& <Alert>Ciudad no encontrada..!!</Alert>}
       </div>
     </>
   )
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { z } from "zod"
 import { SearchType } from "../types"
+import { useMemo, useState } from "react"
+// import { object, string, number, InferOutput, parse } from "valibot"
 
 // TYPE GUARD O ASSERTION
 // function isWWeatherResponse(weather: unknown): weather is Weather {
@@ -24,14 +26,51 @@ const Weather = z.object({
   }),
 })
 
-type Weather = z.infer<typeof Weather>
+export type Weather = z.infer<typeof Weather>
+
+// Valibot
+// const WeatherSchema = object({
+//   name: string(),
+//   main: object({
+//     temp: number(),
+//     temp_max: number(),
+//     temp_min: number(),
+//   }),
+// })
+
+// type Weather = InferOutput<typeof WeatherSchema>
+
+const initialState = {
+  name: "",
+  main: {
+    temp: 0,
+    temp_max: 0,
+    temp_min: 0,
+  },
+}
 
 export default function useWeather() {
-  const appId = import.meta.env.VITE_API_KEY
+  const [weather, setWeather] = useState<Weather>(initialState)
+  const [loading, setLoading] = useState(false)
+  const [notfound, setNotFound] = useState(false)
+
   const fetchWeather = async (search: SearchType) => {
+    const appId = import.meta.env.VITE_API_KEY
+
+    setWeather(initialState)
+    setNotFound(false)
+    setLoading(true)
+    
     try {
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
       const { data } = await axios(geoUrl)
+
+      // Comprobar si existe
+      if (!data[0]) {
+        setNotFound(true)
+        return
+      }
+
       const lat = data[0].lat
       const lon = data[0].lon
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
@@ -50,12 +89,25 @@ export default function useWeather() {
       const { data: weatherResult } = await axios<Weather>(weatherUrl)
       const result = Weather.safeParse(weatherResult)
       if (result.success) {
-        console.log(result.data.name)
-        console.log(result.data.main.temp)
+        setWeather(result.data)
       }
+
+      // Valibot
+      // const { data: weatherResult } = await axios<Weather>(weatherUrl)
+      // const result = parse(WeatherSchema, weatherResult)
+      // if (result) {
+      //   console.log(result.name)
+      // } else {
+      //   console.log("Respuesta mal formada..")
+      // }
     } catch (error) {
       console.log(error)
+    } finally {
+      setLoading(false)
     }
   }
-  return { fetchWeather }
+
+  const hasWeatherData = useMemo(() => weather.name, [weather])
+
+  return { weather, loading, notfound, fetchWeather, hasWeatherData }
 }
