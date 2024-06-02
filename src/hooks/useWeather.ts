da1c7f9e232d5ae08e25c5a2754ffaadabddd0de
Joss Rider <jossrider@outlook.com>
2024-06-01 23:25:03 -0400

    Actualizacion direccion de la api

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -62,7 +62,7 @@ export default function useWeather() {
     setLoading(true)
     
     try {
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
       const { data } = await axios(geoUrl)
 
       // Comprobar si existe
